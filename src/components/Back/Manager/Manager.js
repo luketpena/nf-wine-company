4df70f6a76b21e3988c25a582531c0ed613ae9fb
Luke Peña <luketpena@gmail.com>
2020-01-20 09:17:38 -0600

    Manager styled component

--- src/components/Back/Manager/Manager.js
+++ src/components/Back/Manager/Manager.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Manager.css';
 import { Switch } from 'react-router'; 
 import { Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 //-----< Component Imports >-----\\
 import ManagerMenu from './ManagerMenu';
@@ -9,16 +10,26 @@ import EventLanding from './0-Events/EventLanding';
 import SupplierLanding from './1-Suppliers/SupplierLanding';
 import EventInput from './0-Events/EventInput';
 
+//-----< Styling >-----\\
+const Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background-color: rgb(248, 245, 238);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 //-----< Component Function >-----\\
 export default function Manager () {
   return (
-    <div className="managerBox">
+    <Container>
         <Switch>
           <Route exact path="/manager" component={ManagerMenu}/>
           <Route exact path="/manager/events" component={EventLanding} />
           <Route exact path="/manager/suppliers" component={SupplierLanding} />
           <Route path="/manager/events/update/:action" component={EventInput} />
         </Switch>
-    </div>
+    </Container>
   )
 }
