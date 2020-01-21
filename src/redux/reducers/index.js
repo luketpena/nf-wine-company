d8edc3464508bf1497a4dd8a34d35638f3d36b5a
Luke Peña <luketpena@gmail.com>
2020-01-21 11:32:28 -0600

    Authentication time: installed dependencies and brought in reducers, sagas, and components

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import edit from './edit.redux';
 import event from './event.redux';
 import places from './places.redux';
 import supplier from './supplier.redux';
+import user from './user.redux';
 
 const rootReducer = combineReducers({
   edit,
   event,
   places,
   supplier,
+  user,
 });
 
 export default rootReducer;
