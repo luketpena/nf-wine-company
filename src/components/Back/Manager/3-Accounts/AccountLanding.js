a7fdacffbd8c320dc69ad7504d83d0d7b81bae61
Luke Peña <luketpena@gmail.com>
2020-01-21 14:24:00 -0600

    Created route to get user information

--- src/components/Back/Manager/3-Accounts/AccountLanding.js
+++ src/components/Back/Manager/3-Accounts/AccountLanding.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
+import AccountInfo from './AccountInfo';
+import AccountUser from './AccountUser';
+import AccountRequest from './AccountRequest';
 
+const Container = styled.div`
+  display: grid;
+  grid-template-areas: "info request" "user request";
+  grid-template-columns: 250px 1fr;
 `;
 
 export default function AccountLanding() {
   return (
     <Container>
       
+      <AccountInfo />
+      <AccountUser />
+      <AccountRequest />
     </Container>
   )
 }
