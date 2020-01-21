ab9e0ce679324a4993614957c735e32202d1e9c2
Luke Peña <luketpena@gmail.com>
2020-01-21 13:59:39 -0600

    Set up account page

---
+++ src/components/Back/Manager/3-Accounts/AccountRequest.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  grid-area: request
+  background-color: yellow;
+`;
+
+export default function AccountRequest() {
+
+  return (
+    <Container>
+
+    </Container>
+  )
+}
