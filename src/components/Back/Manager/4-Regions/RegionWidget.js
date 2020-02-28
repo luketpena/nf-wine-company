216b754386b60cbc1f0f3abbddfb4de03f73e881
Luke Peña <luketpena@gmail.com>
2020-02-28 13:01:47 -0600

    Rendering buttons in regionWidgets + input states

--- src/components/Back/Manager/4-Regions/RegionWidget.js
+++ src/components/Back/Manager/4-Regions/RegionWidget.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
 import styled from 'styled-components';
 
@@ -30,8 +30,40 @@ export default function RegionWidget(props) {
   const {id, name, region_code, country_id} = props.region;
   const dispatch = useDispatch();
 
+  let [edit, setEdit] = useState(false);
+  let [name_in, setName_in] = useState(name);
+  let [code_in, setCode_in] = useState(region_code);
+
   function deleteRegion() {
     dispatch({type: 'DELETE_REGION', payload: {region_id: id, country_id}});
+  }
+
+  function submitEdit() {
+
+  }
+
+  function cancelEdit() {
+    setName_in(name);
+    setCode_in(region_code);
+    setEdit(false);
+  }
+
+  function renderButtons() {
+    if (edit) {
+      return (
+        <>
+          <button className="button-confirm" onClick={submitEdit}>Submit</button>
+          <button className="button-default" onClick={cancelEdit}>Cancel</button>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <button className="button-default" onClick={()=>setEdit(true)}>Edit</button>
+          <button className="button-primary" onClick={deleteRegion}>Delete</button>
+        </>
+      )
+    }
   }
 
   return (
@@ -42,8 +74,7 @@ export default function RegionWidget(props) {
       </InfoBox>
 
       <ButtonBox>
-        <button className="button-default">Edit</button>
-        <button className="button-primary" onClick={deleteRegion}>Delete</button>
+        {renderButtons()}
       </ButtonBox>
 
     </Container>
