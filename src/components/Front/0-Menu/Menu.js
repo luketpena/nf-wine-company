161874e5e87f90e6ef87f663e5b209a373b531c3
Luke Peña <luketpena@gmail.com>
2020-03-07 18:25:59 -0600

    Created hamburger menu basics

--- src/components/Front/0-Menu/Menu.js
+++ src/components/Front/0-Menu/Menu.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import getOffsetTop from '../../../modules/getOffsetTop';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
 
-const Container = styled.nav`
+const ContainerDesktop = styled.nav`
   
   line-height: 64px;
   height: 64px;
@@ -42,15 +43,49 @@ const Container = styled.nav`
   }
 `;
 
+const ContainerMobile = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0; 
+  z-index: 10;
+  width: ${props=>(props.active? '100%' : '48px')};
+  height: 100vh;
+  transition: width .5s;
+
+  .icon {
+    color: white;
+    font-size: 2em;
+    margin: 8px;
+    position: absolute;
+    text-align: right;
+    top: 0;
+    right: 0;
+    z-index: 11;
+  }
+
+  .menu-list {
+    background-color: rgba(165, 42, 42, .9);
+    backdrop-filter: blur(16px);
+    height: 100%;
+    position: relative;
+    transition: right .5s;
+    right: ${props=>(props.active? '0' : '48')}px;
+  }
+`;
+
 export default function Menu() {
 
   let [scrollY, setScrollY] = useState(0);
   let [mount, setMount] = useState(false);
+  let [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  
+  let [menuActive, setMenuActive] = useState(false);
 
   useEffect(()=>{
     if (!mount) {
       setMount(true);
       window.addEventListener('scroll',handleScroll,true);
+      window.addEventListener('resize',()=>setScreenWidth(window.innerWidth));
     }
   },[scrollY, mount]);
 
@@ -58,18 +93,41 @@ export default function Menu() {
     setScrollY(window.scrollY);
   }
 
+  function renderMenu() {
+    if (screenWidth>800) {
+      return (
+        <ContainerDesktop id="menu" scrollY={scrollY} point={window.innerHeight*.25}>
+          <nav>
+            <ul>
+              <li>NF</li>
+              <li>About</li>
+              <li>Events</li>
+              <li>Suppliers</li>
+              <li>Producer</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </ContainerDesktop>
+      )
+    } else {
+      return (
+        <ContainerMobile active={menuActive}>
+          <FontAwesomeIcon 
+            className="icon" 
+            icon={(menuActive? faTimes : faBars)}
+            onClick={()=>setMenuActive(!menuActive)}
+          />
+          <div className="menu-list">
+
+          </div>
+        </ContainerMobile>
+      )
+    }
+  }
+
   return (
-    <Container id="menu" scrollY={scrollY} point={window.innerHeight*.25}>
-      <nav>
-        <ul>
-          <li>NF</li>
-          <li>About</li>
-          <li>Events</li>
-          <li>Suppliers</li>
-          <li>Producer</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </Container>
+    <>
+      {renderMenu()}
+    </>
   )
 }
