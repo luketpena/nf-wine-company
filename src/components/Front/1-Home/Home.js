05809b30c4bf66471b40ca71332fd3d4599635cf
Luke Peña <luketpena@gmail.com>
2020-03-09 17:15:28 -0500

    Added links for landing buttons

--- src/components/Front/1-Home/Home.js
+++ src/components/Front/1-Home/Home.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {useHistory} from 'react-router-dom';
 import styled from 'styled-components';
 
 import Landing from './Landing/Landing';
@@ -29,35 +30,34 @@ const GoButton = styled.a`
 
 
 
-class Home extends Component {
-  render() {
-    return (
-      <main>
-        <Landing/>
+export default function Home() {
 
-        <Section className="sec-default">
-          <h2>Who We Are</h2>
-          <p>We're a small company that specializes in niche producers who make great wine. New France Wine Company has been in Minnesota since 1990; previously, Robert Rudolph, owner and founder, owned a distributorship/import wine company in San Francisco.</p>
-          <p>We're wine people first and salespeople second; we hand-sell all of our wine, working with retailers and restaurants to find what's right for them.</p>
-          <GoButton>Learn More About Us</GoButton>
-        </Section>
+  const history = useHistory();
 
-        <Section className="sec-default">
-          <h2>What We're Doing</h2>
-          <p>"We don't just supply wine to x number of business - we also host events for, both here and around the Twin Cities."</p>
-          <GoButton>See Upcoming Events</GoButton>
-        </Section>
+  return (
+    <main>
+      <Landing/>
 
-        <Section className="sec-default">
-          <h2>Who We Work With</h2>
-          <p>New France Wine Company has a large and eclectic portfolio, focusing on France, Italy, and Spain, but with an eye toward value in all parts of the world, including domestic, Portugal, Australia, and elsewhere.</p>
-          <p>All of our imported wines are domaine-bottled, and we strive to work with producers who are the best at their craft.</p>
-          <GoButton>View Our Partners</GoButton>
-        </Section>
- 
-      </main>
-    )
-  }
+      <Section className="sec-default">
+        <h2>Who We Are</h2>
+        <p>We're a small company that specializes in niche producers who make great wine. New France Wine Company has been in Minnesota since 1990; previously, Robert Rudolph, owner and founder, owned a distributorship/import wine company in San Francisco.</p>
+        <p>We're wine people first and salespeople second; we hand-sell all of our wine, working with retailers and restaurants to find what's right for them.</p>
+        <GoButton onClick={()=>history.push('/about')}>Learn More About Us</GoButton>
+      </Section>
+
+      <Section className="sec-default">
+        <h2>What We're Doing</h2>
+        <p>"We don't just supply wine to x number of business - we also host events for, both here and around the Twin Cities."</p>
+        <GoButton onClick={()=>history.push('/events')}>See Upcoming Events</GoButton>
+      </Section>
+
+      <Section className="sec-default">
+        <h2>Who We Work With</h2>
+        <p>New France Wine Company has a large and eclectic portfolio, focusing on France, Italy, and Spain, but with an eye toward value in all parts of the world, including domestic, Portugal, Australia, and elsewhere.</p>
+        <p>All of our imported wines are domaine-bottled, and we strive to work with producers who are the best at their craft.</p>
+        <GoButton onClick={()=>history.push('/partners')}>View Our Partners</GoButton>
+      </Section>
+
+    </main>
+  )
 }
-
-export default Home;
