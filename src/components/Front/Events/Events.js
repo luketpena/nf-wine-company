4183168e6d3fd84fec1e09ccdefc8f56c3677d16
Luke Peña <luketpena@gmail.com>
2020-03-18 18:47:23 -0500

    Started html and css for event page and items

---
+++ src/components/Front/Events/Events.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+
+const EventBox = styled.div`
+
+`;
+
+const eventList = [
+  {
+    name: 'event 1',
+    date: '03/30/2020',
+    location: 'My place',
+    time: '17:00'
+  },
+  {
+    name: 'event 2',
+    date: '03/30/2020',
+    location: 'My place',
+    time: '17:00'
+  }
+]
+
+export default function Events() {
+
+  function renderEvents(array) {
+    if (array.length>0) {
+      return eventList.map( (item,i)=>{
+        
+      })
+    } else {
+      return (
+        <p>There are currently no upcoming events.</p>
+      )
+    }
+  }
+
+  return (
+    <Container>
+      <h1>Events</h1>
+      <EventBox className="sec-default">
+        {renderEvents(eventList)}
+      </EventBox>
+    </Container>
+  )
+}
