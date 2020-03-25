02564586d61e1e98728a8fe214cbccd3698bf5bc
Luke Peña <luketpena@gmail.com>
2020-03-24 20:22:13 -0500

    Added separators on menu with static background

--- src/components/Front/Partners/ProducerList.js
+++ src/components/Front/Partners/ProducerList.js
@@ -5,6 +5,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCaretUp, faCaretDown} from '@fortawesome/free-solid-svg-icons';
 
 import ProducerItem from './ProducerItem';
+import Modal from '../../GenUse/Modal/Modal';
 
 const Container = styled.div`
   margin-top: 0;
@@ -70,6 +71,25 @@ const SortText = styled.span`
   }
 `;
 
+const ModalContent = styled.div`
+
+  h3 {
+    font-family: var(--font-title);
+    font-size: 2em;
+    border-bottom: 1px solid gray;
+    text-align: center;
+    margin: 16px 0 0 0;
+  }
+  .location {
+    color: #AAA;
+    margin: 0;
+    text-align: center;
+    .divide {
+      margin: 0 16px;
+    }
+  }
+`;
+
 export default function ProducerList(props) {
 
   const dispatch = useDispatch();
@@ -78,6 +98,8 @@ export default function ProducerList(props) {
 
   let [pageSelect, setPageSelect] = useState(1);
   let [pageStart,setPageStart] = useState(0);
+  let [open, setOpen] = useState(false);
+  let [targetProducer, setTargetProducer] = useState({});
 
   const {search, setSearch, countryFilter, regionFilter, subregionFilter, setSort, sort, setOrder, order} = props;
 
@@ -123,9 +145,24 @@ export default function ProducerList(props) {
     let returnArray = [];
 
       for (let i=pageStart; i<Math.min(copyArray.length,pageStart+paginationUnit); i++) {     
-        returnArray.push(<ProducerItem producer={copyArray[i]} key={i}/>)
+        returnArray.push(<ProducerItem producer={copyArray[i]} key={i} setTargetProducer={setTargetProducer} setOpen={setOpen}/>)
       }
     return returnArray;
+  }
+  
+  function renderModalContents() {
+    return (
+      <div>
+        {(targetProducer.name? <h3>{targetProducer.name}</h3> : <></>)}
+        <p className="location">
+          {(targetProducer.country_name? <span>{targetProducer.country_name}</span> : <></>)}
+          {(targetProducer.country_name && targetProducer.region_name? <span className="divide">-</span> : <></>)}
+          {(targetProducer.region_name? <span>{targetProducer.region_name}</span> : <></>)}
+        </p>
+        {(targetProducer.description? <p className="description">{targetProducer.description}</p> : <></>)}
+        {(targetProducer.website_url? <button className="button-front">Visit them online</button> : <></>)}
+      </div>
+    )
   }
 
   return (
@@ -159,6 +196,12 @@ export default function ProducerList(props) {
           {renderProducers()}
         </tbody>
       </ProducerTable>
+
+      <Modal open={open} handleClose={()=>setOpen(false)}>
+        <ModalContent>
+          {renderModalContents()}
+        </ModalContent>
+      </Modal>
     </Container>
   )
 }
