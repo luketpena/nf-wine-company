056c0b43d7df0b962031ee3c844eecdeda0ada24
Luke Peña <luketpena@gmail.com>
2020-03-25 12:25:24 -0500

    Created logout button for trade portal

--- src/components/Back/Customer/TradeView.js
+++ src/components/Back/Customer/TradeView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import styled from 'styled-components';
 
 import EventList from '../../GenUse/EventList/EventList';
@@ -15,12 +15,19 @@ const Container = styled.div`
 
 export default function TradeView() {
 
+  const dispatch = useDispatch();
+
   const user = useSelector(state=>state.user);
+
+  function logout() {
+    dispatch({type: 'LOGOUT'});
+  }
 
   return (
     <Container>
       <FrontLanding title={`Hello, ${user.username}`}/>
       <button className="button-secondary">Download pricing PDF</button>
+      <button className="button-default" onClick={logout}>Logout</button>
       <EventList/>
     </Container>
   )
