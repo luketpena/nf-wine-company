c91fd7d9821604912e4b5b9bb1180491843cd24c
Luke Peña <luketpena@gmail.com>
2020-03-26 12:50:50 -0500

    Updated event widget styling

--- src/components/Back/Manager/0-Events/EventLanding.js
+++ src/components/Back/Manager/0-Events/EventLanding.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import TravelButton from '../../../GenUse/TravelButton/TravelButton';
 import EventWidget from './EventWidget';
 import { useSelector } from 'react-redux';
 
 /*-----< Component Imports >-----*/
 import ManagerTitle from '../ManagerTitle';
+
+/*-----< Styling >-----*/
+const NoEventsMessage = styled.p`
+  text-align: center;
+  font-size: 1em;
+`;
 
 /*-----< Component Function >-----*/
 export default function EventLanding() {
@@ -22,9 +29,16 @@ export default function EventLanding() {
       if ( (eventDate<dateNow && timing==='past') || (eventDate>dateNow && timing==='upcoming') ) {
         return <EventWidget key={event.id} event={event} index={i}/>
       }
-      return false;
-    });
-    return eventList;
+    }).filter(event=>event);
+
+    console.log('The current eventList:',eventList,timing);
+    
+
+    if (eventList.length>0) {
+      return eventList;
+    } else {
+      return <NoEventsMessage>There are no {timing} events.</NoEventsMessage>
+    }
   }
 
   return (
