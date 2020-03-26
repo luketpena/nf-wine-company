d59e10e912669ef9a16cb46759ae7e69b8120d29
Luke Peña <luketpena@gmail.com>
2020-03-26 12:02:41 -0500

    Removed back button from manager title

--- src/components/Back/Manager/ManagerTitle.js
+++ src/components/Back/Manager/ManagerTitle.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import TravelButton from '../../GenUse/TravelButton/TravelButton';
 
 export default function ManagerTitle(props) {
   return (
     <div className="header-bar">
       <h1>{props.title}</h1>
-      <TravelButton target={props.target} text="Back" propClass='button-default'/>
     </div>
   )
 }
