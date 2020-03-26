7b5907cb7d650d6e5dc679a3b466a9d69ab79b5a
Luke Peña <luketpena@gmail.com>
2020-03-26 11:35:14 -0500

    Removed login errorbar and replaced with use of modal

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import styled from 'styled-components';
-import LoginErrorBar from './LoginErrorBar';
 import LoginInput from './LoginInput';
 
 import barrels from '../../images/bkg-barrels.jpg';
+
+import Modal from '../GenUse/Modal/Modal';
 
 const InputBox = styled.div`
   position: relative;
   overflow: hidden;
   grid-area: main;
-  width: 100%;
+  width: max-content;
   max-width: 512px;
   margin: 0 auto;
   border-radius: 16px;
   padding: 32px;
-  background-color: #DDD;
+  background-color: white;
+
+  h1 {
+    color: var(--col-primary);
+    text-shadow: none;
+    margin: 0 auto;
+  }
 `;
 
 const Container = styled.div`
@@ -30,23 +37,25 @@ const Container = styled.div`
   background-attachment: fixed;
 `;
 
+const ErrorMessage = styled.p`
+  font-size: 1.25em;
+  text-align: center;
+`;
+
 export default function RegisterPage() {
 
+  const dispatch = useDispatch();
   let errors = useSelector(state=>state.errors);
-
-  function renderErrorBar() {
-    if (errors.registrationMessage || errors.loginMessage) {
-      return <LoginErrorBar />
-    }
-  }
 
   return (
     <Container className="hello">
       <InputBox>
-        {renderErrorBar()}
         <h1 className="logReg-title">Login</h1>
         <LoginInput />
       </InputBox>
+      <Modal open={errors.loginMessage} handleClose={()=>dispatch({type: 'CLEAR_LOGIN_ERROR'})}>
+        <ErrorMessage>{errors.loginMessage}</ErrorMessage>
+      </Modal>
     </Container>
   );
 }
