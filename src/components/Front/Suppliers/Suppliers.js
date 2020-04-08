e91285512c230edcc8d4c57ac05220a42349ede7
Luke Peña <luketpena@gmail.com>
2020-04-07 19:25:32 -0500

    Added base supplier page

---
+++ src/components/Front/Suppliers/Suppliers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/*-----< Component Imports >-----*/
+import FrontLanding from '../FrontLanding';
+
+export default function Suppliers() {
+
+  return (
+    <div>
+      <FrontLanding 
+        title="Suppliers" 
+        text="[Here you can talk about the people you partner with to get your supply,]"/>
+    </div>
+  )
+}
