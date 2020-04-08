4af4288fcdf8a56a25b9ff427672c9a7548496c3
Luke Peña <luketpena@gmail.com>
2020-04-08 12:13:39 -0500

    Suppliers rendered to the list

---
+++ src/components/Front/Suppliers/SupplierListItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SupplierListItem(props) {
+  return (
+    <li>{props.supplier.name}</li>
+  )
+}
--- src/components/Front/Suppliers/SupplierListSegment.js
+++ src/components/Front/Suppliers/SupplierListSegment.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import SupplierListItem from './SupplierListItem';
+
 export default function SupplierListSegment(props) {
+
+  function renderSuppliers() {
+    return props.suppliers.map( (supplier,i)=>{
+      return <SupplierListItem key={i} supplier={supplier} />
+    });
+  }
+
   return (
     <div>
       <h3>{props.letter}</h3>
+      <ul>
+        {renderSuppliers()}
+      </ul>
     </div>
   )
 }
