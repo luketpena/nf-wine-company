81e3e0b0193e56ce3ab5946e45146c33938fb2eb
Luke Peña <luketpena@gmail.com>
2020-04-30 08:23:05 -0500

    Added new background

--- src/components/Front/Home/Landing/Landing.js
+++ src/components/Front/Home/Landing/Landing.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import styled, {keyframes} from 'styled-components';
-import background from './vineyard.mp4';
+import background from './background.mp4';
 
 const fadeIn = keyframes`
   from {opacity: 0;}
