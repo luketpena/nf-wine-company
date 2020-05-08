5783f8daeff94342932c76b95be368586bd7cf33
Luke Peña <luketpena@gmail.com>
2020-05-08 09:54:06 -0500

    Reverting to hashrouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {connect} from 'react-redux';
 //Navigation Imports
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route} from 'react-router-dom';
 import { Switch } from 'react-router';
 //Component imports
 import Manager from '../Back/Manager/Manager';
@@ -26,7 +26,7 @@ class App extends Component {
       <div>
         <Router>
           <Switch>
-            <ProtectedRoute path="/manager" component={Manager}/>
+            <Route path="/manager" component={Manager}/>
             <Route path="/tradeportal" component={TradePortal}/>
             <Route path="/" component={Public}/>
             <Route path="/scraper" component={CountryScraper}/>
