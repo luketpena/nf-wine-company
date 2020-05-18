0203596a20061059ffd2745afad78b39e82eaf67
Luke Peña <luketpena@gmail.com>
2020-05-18 11:25:06 -0500

    Social media links added to footer

--- src/components/GenUse/Footer/Footer.js
+++ src/components/GenUse/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import {faFacebookSquare, faInstagramSquare} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const Container = styled.footer`
   background-color: var(--col-primary);
@@ -12,10 +14,32 @@ const Container = styled.footer`
   }
 `;
 
+const IconTray = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  cursor: pointer;
+
+  .icon {
+    margin: 0 8px;
+    font-size: 32px;
+    color: white;
+    text-decoration: none;
+  }
+`;
+
 export default function Footer() {
   return (
     <Container>
       <p>Copyright © {new Date().getFullYear()} New France Wine Company. All rights reserved.</p>
+      <IconTray>
+        <a>
+          <FontAwesomeIcon className="icon" icon={faFacebookSquare} />
+        </a>
+        <a href="https://www.instagram.com/newfrancewine/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className="icon" icon={faInstagramSquare} />
+        </a>
+      </IconTray>
     </Container>
   )
 }
