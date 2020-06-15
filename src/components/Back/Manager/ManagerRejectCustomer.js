a817e4031697266218f860b3d2cb3e6be9f783d6
Luke Peña <luketpena@gmail.com>
2020-06-15 17:58:39 -0500

    Added content for customer manager rejection page

--- src/components/Back/Manager/ManagerRejectCustomer.js
+++ src/components/Back/Manager/ManagerRejectCustomer.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import styled from 'styled-components';
+import LogOutButton from '../../LogOutButton/LogOutButton';
+
 
 const Container = styled.div`
-  background-color: blue;
-  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+
+  .contentHouse {
+    height: max-content;
+  }
+
+  .contentBox {
+    max-width: 700px;
+  }
+
+  button {
+    background-color: var(--col-primary);
+    color: white;
+    border: none;
+    outline: none;
+    padding: 16px 48px;
+    font-size: 1em;
+    border-radius: 16px;
+    transition: padding .2s;
+    cursor: pointer;
+    &:hover {
+      padding: 16px 64px;
+    }
+  }
 `;
 
 export default function ManagerRejectCustomer() {
 
+  const dispatch = useDispatch();
+
   return (
     <Container>
-      GET OUT.
+      <div className="sec-default contentHouse">
+        <div className="sec-default-content">
+          <div className="contentBox">
+            <p>The account you are currently logged into has customer-level access only. Please log out and try again with an account that has admin level access.</p>
+            <LogOutButton/>
+          </div>
+        </div>
+      </div>  
     </Container>
   )
 }
