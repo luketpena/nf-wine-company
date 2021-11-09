d32064ca0cf65def7bd32be804e845912e278d53
Luke <luketpena@gmail.com>
2021-11-08 21:41:55 -0600

    chore: add logs to authentication middleware

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -3,9 +3,11 @@ const rejectUnauthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     // They were authenticated! User may do the next thing
     // Note! They may not be Authorized to do all things
+    console.log('User is authenticated');
     next();
   } else {
     // failure best handled on the server. do redirect here.
+    console.error('User not authenticated!');
     res.sendStatus(403);
   }
 };
