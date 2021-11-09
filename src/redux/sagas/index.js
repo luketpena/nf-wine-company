507092aa35e7a810c1300e3d8868fca4b138fc15
Luke <luketpena@gmail.com>
2021-11-09 08:24:33 -0600

    add back places saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -16,7 +16,7 @@ export default function* rootSaga() {
   yield all([
     // eventSaga(),
     // editSaga(),
-    // placesSaga(),
+    placesSaga(),
     // supplierSaga(),
     // producerSaga(),
     // loginSaga(),
